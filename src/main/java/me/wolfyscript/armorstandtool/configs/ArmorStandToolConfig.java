2323a9726ffaf12f3e9ac6a48052c5fbe322ab7a
WolfyScript <wolfyscript@gmail.com>
2020-08-20 19:01:27 +0200

    Made the ArmorStandToolConfig compatible with Jackson

--- src/main/java/me/wolfyscript/armorstandtool/configs/ArmorStandToolConfig.java
+++ src/main/java/me/wolfyscript/armorstandtool/configs/ArmorStandToolConfig.java
@@ -4,18 +4,33 @@ import me.wolfyscript.armorstandtool.ArmorStandTool;
 import me.wolfyscript.utilities.api.config.Config;
 import me.wolfyscript.utilities.api.config.ConfigAPI;
 import me.wolfyscript.utilities.api.config.JsonConfiguration;
+import me.wolfyscript.utilities.libraries.com.fasterxml.jackson.annotation.JsonGetter;
+import me.wolfyscript.utilities.libraries.com.fasterxml.jackson.annotation.JsonSetter;
 
-public class ArmorStandToolConfig extends JsonConfiguration {
+public class ArmorStandToolConfig {
 
-    public ArmorStandToolConfig(ConfigAPI configAPI) {
-        super(configAPI, ArmorStandTool.getInstance().getDataFolder().getPath(), "main_config", "me/wolfyscript/armorstandtool/configs", "config", false);
-    }
+    private String lang;
+    private boolean armorStandKnockback;
 
+    @JsonGetter(value = "language")
     public String getLang(){
-        return getString("language");
+        return lang;
     }
 
-    public boolean armorStandKnockback(){
-        return getBoolean("armorstand_knockback");
+    @JsonSetter(value = "language")
+    public void setLang(String lang) {
+        this.lang = lang;
     }
+
+    @JsonGetter(value = "armorstand_knockback")
+    public boolean isArmorStandKnockback() {
+        return armorStandKnockback;
+    }
+
+    @JsonSetter(value = "armorstand_knockback")
+    public void setArmorStandKnockback(boolean armorStandKnockback) {
+        this.armorStandKnockback = armorStandKnockback;
+    }
+
+
 }
