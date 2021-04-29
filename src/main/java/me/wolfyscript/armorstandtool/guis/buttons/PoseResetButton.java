493148ed690d7a5f078967c710c56f9dbc9a34de
WolfyScript <wolfyscript@gmail.com>
2021-04-29 14:59:32 +0200

    Improved PoseResetButton code readability.

--- src/main/java/me/wolfyscript/armorstandtool/guis/buttons/PoseResetButton.java
+++ src/main/java/me/wolfyscript/armorstandtool/guis/buttons/PoseResetButton.java
@@ -1,10 +1,10 @@
 package me.wolfyscript.armorstandtool.guis.buttons;
 
-import me.wolfyscript.armorstandtool.ArmorStandTool;
 import me.wolfyscript.armorstandtool.data.OptionType;
+import me.wolfyscript.armorstandtool.data.ASTCache;
+import me.wolfyscript.armorstandtool.util.ArmorStandUtils;
 import me.wolfyscript.utilities.api.inventory.gui.button.ButtonState;
 import me.wolfyscript.utilities.api.inventory.gui.button.buttons.ActionButton;
-import me.wolfyscript.utilities.api.inventory.gui.cache.CustomCache;
 import org.bukkit.Material;
 import org.bukkit.entity.ArmorStand;
 
@@ -15,76 +15,25 @@ public class PoseResetButton extends ActionButton<CustomCache> {
             ArmorStand stand = ArmorStandTool.getPlayerCache(player).getArmorStand();
             switch (ArmorStandTool.getPlayerCache(player).getCurrentOption()) {
                 case ROTATION_RIGHT_LEG:
-                    switch (xyz) {
-                        case "x":
-                            stand.setRightLegPose(stand.getRightLegPose().setX(0));
-                            break;
-                        case "y":
-                            stand.setRightLegPose(stand.getRightLegPose().setY(0));
-                            break;
-                        case "z":
-                            stand.setRightLegPose(stand.getRightLegPose().setZ(0));
-                    }
+                    stand.setRightLegPose(ArmorStandUtils.resetRotation(stand.getRightLegPose(), xyz));
                     break;
                 case ROTATION_RIGHT_ARM:
-                    switch (xyz) {
-                        case "x":
-                            stand.setRightArmPose(stand.getRightArmPose().setX(0));
-                            break;
-                        case "y":
-                            stand.setRightArmPose(stand.getRightArmPose().setY(0));
-                            break;
-                        case "z":
-                            stand.setRightArmPose(stand.getRightArmPose().setZ(0));
-                    }
+                    stand.setRightArmPose(ArmorStandUtils.resetRotation(stand.getRightArmPose(), xyz));
                     break;
                 case ROTATION_LEFT_LEG:
-                    switch (xyz) {
-                        case "x":
-                            stand.setLeftLegPose(stand.getLeftLegPose().setX(0));
-                            break;
-                        case "y":
-                            stand.setLeftLegPose(stand.getLeftLegPose().setY(0));
-                            break;
-                        case "z":
-                            stand.setLeftLegPose(stand.getLeftLegPose().setZ(0));
-                    }
+                    stand.setLeftLegPose(ArmorStandUtils.resetRotation(stand.getLeftLegPose(), xyz));
                     break;
                 case ROTATION_LEFT_ARM:
-                    switch (xyz) {
-                        case "x":
-                            stand.setLeftArmPose(stand.getLeftArmPose().setX(0));
-                            break;
-                        case "y":
-                            stand.setLeftArmPose(stand.getLeftArmPose().setY(0));
-                            break;
-                        case "z":
-                            stand.setLeftArmPose(stand.getLeftArmPose().setZ(0));
-                    }
+                    stand.setLeftArmPose(ArmorStandUtils.resetRotation(stand.getLeftArmPose(), xyz));
                     break;
                 case ROTATION_HEAD:
-                    switch (xyz) {
-                        case "x":
-                            stand.setHeadPose(stand.getHeadPose().setX(0));
-                            break;
-                        case "y":
-                            stand.setHeadPose(stand.getHeadPose().setY(0));
-                            break;
-                        case "z":
-                            stand.setHeadPose(stand.getHeadPose().setZ(0));
-                    }
+                    stand.setHeadPose(ArmorStandUtils.resetRotation(stand.getHeadPose(), xyz));
                     break;
                 case ROTATION_BODY:
-                    switch (xyz) {
-                        case "x":
-                            stand.setBodyPose(stand.getBodyPose().setX(0));
-                            break;
-                        case "y":
-                            stand.setBodyPose(stand.getBodyPose().setY(0));
-                            break;
-                        case "z":
-                            stand.setBodyPose(stand.getBodyPose().setZ(0));
-                    }
+                    stand.setBodyPose(ArmorStandUtils.resetRotation(stand.getBodyPose(), xyz));
+                    break;
+                default:
+                    //No setting!
             }
             return true;
         }, (hashMap, customCache, guiHandler, player, guiInventory, itemStack, i, b) -> {
@@ -96,65 +45,19 @@ public class PoseResetButton extends ActionButton<CustomCache> {
     private static double getPose(OptionType optionType, String xyz, ArmorStand stand) {
         switch (optionType) {
             case ROTATION_RIGHT_LEG:
-                switch (xyz) {
-                    case "x":
-                        return stand.getRightLegPose().getX();
-                    case "y":
-                        return stand.getRightLegPose().getY();
-                    case "z":
-                        return stand.getRightLegPose().getZ();
-                }
-                break;
+                return ArmorStandUtils.getPose(stand.getRightLegPose(), xyz);
             case ROTATION_RIGHT_ARM:
-                switch (xyz) {
-                    case "x":
-                        return stand.getRightArmPose().getX();
-                    case "y":
-                        return stand.getRightArmPose().getY();
-                    case "z":
-                        return stand.getRightArmPose().getZ();
-                }
-                break;
+                return ArmorStandUtils.getPose(stand.getRightArmPose(), xyz);
             case ROTATION_LEFT_LEG:
-                switch (xyz) {
-                    case "x":
-                        return stand.getLeftLegPose().getX();
-                    case "y":
-                        return stand.getLeftLegPose().getY();
-                    case "z":
-                        return stand.getLeftLegPose().getZ();
-                }
-                break;
+                return ArmorStandUtils.getPose(stand.getLeftLegPose(), xyz);
             case ROTATION_LEFT_ARM:
-                switch (xyz) {
-                    case "x":
-                        return stand.getLeftArmPose().getX();
-                    case "y":
-                        return stand.getLeftArmPose().getY();
-                    case "z":
-                        return stand.getLeftArmPose().getZ();
-                }
-                break;
+                return ArmorStandUtils.getPose(stand.getLeftArmPose(), xyz);
             case ROTATION_HEAD:
-                switch (xyz) {
-                    case "x":
-                        return stand.getHeadPose().getX();
-                    case "y":
-                        return stand.getHeadPose().getY();
-                    case "z":
-                        return stand.getHeadPose().getZ();
-                }
-                break;
+                return ArmorStandUtils.getPose(stand.getHeadPose(), xyz);
             case ROTATION_BODY:
-                switch (xyz) {
-                    case "x":
-                        return stand.getBodyPose().getX();
-                    case "y":
-                        return stand.getBodyPose().getY();
-                    case "z":
-                        return stand.getBodyPose().getZ();
-                }
+                return ArmorStandUtils.getPose(stand.getBodyPose(), xyz);
+            default:
+                return 0d;
         }
-        return 0.0;
     }
 }
--- src/main/java/me/wolfyscript/armorstandtool/util/ArmorStandUtils.java
+++ src/main/java/me/wolfyscript/armorstandtool/util/ArmorStandUtils.java
@@ -7,8 +7,11 @@ import org.bukkit.Location;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.util.EulerAngle;
 
 public class ArmorStandUtils {
+
+    private ArmorStandUtils(){ }
 
     public static boolean teleportStand(ArmorStand stand, Location location, Player player) {
         if (WolfyUtilities.hasPlotSquared() && PSUtils.isPlotWorld(location.getWorld())) {
@@ -24,6 +27,23 @@ public class ArmorStandUtils {
             return true;
         }
 
+    }
+
+    public static EulerAngle resetRotation(EulerAngle pose, String xyz) {
+        return new EulerAngle(xyz.equals("x") ? 0 : pose.getX(), xyz.equals("y") ? 0 : pose.getY(), xyz.equals("z") ? 0 : pose.getZ());
+    }
+
+    public static double getPose(EulerAngle pose, String xyz) {
+        switch (xyz) {
+            case "x":
+                return pose.getX();
+            case "y":
+                return pose.getY();
+            case "z":
+                return pose.getZ();
+            default:
+                return 0d;
+        }
     }
 
 
